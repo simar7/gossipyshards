4d1bbb667c22580057640a9b701dfd7d2d7fa4c5
Simarpreet Singh <simar@linux.com>
2017-06-17 00:21:05 -0700

    bigbang: Sample client-server RPC example
    Signed-off-by: Simarpreet Singh <simar@linux.com>

---
+++ server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/rpc"
+)
+
+type Listener int
+
+func (l *Listener) GetLine(line []byte, ack *bool) error {
+	fmt.Println(string(line))
+	return nil
+}
+
+func main() {
+	addr, err := net.ResolveTCPAddr("tcp", "0.0.0.0:8001")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	inbound, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	listener := new(Listener)
+	rpc.Register(listener)
+	rpc.Accept(inbound)
+}
